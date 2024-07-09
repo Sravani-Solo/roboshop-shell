00f5a25743718bd00ebb265144a65cd0fce0a801
Sravani-Solo <sravani.kotyada@outlook.com>
2024-07-09 22:31:09 +0530

    MonoDB Installation

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="\tmp\$0-$TIMESTAMP.log"
+
+echo -e "Script started at $Y $TIMESTAMP $N" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R Failed $N"
+        exit 1
+    else
+        echo -e "$2 ... $G Success $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copy mongo.repo"
+
+if [ $? -ne 0 ]
+then
+    echo -e "$R ERROR: Run this script with root user $N"
+    exit 1
+else
+    if rpm -q mongodb-org
+    then
+        echo -e "MongoDB already Installed ... $Y SKIPPING $N"
+        exit 1
+    else
+        echo -e "$Y Installing MongoDB ... $N"
+        dnf install mongodb-org -y
+        VALIDATE $? "Installing MongoDB" &>> $LOGFILE
+    fi
+fi
+
+systemctl enable mongod
+VALIDATE $? "Enabling MongoDB" &>> $LOGFILE
+systemctl start mongod
+VALIDATE $? "Starting MongoDB" &>> $LOGFILE
+sed -i '127.01.0/0.0.0.0/g' /etc/mongod.config &>> $LOGFILE
+VALIDATE $? "Allowing MongoDB to listen on all interfaces" &>> $LOGFILE
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB" &>> $LOGFILE
+    
